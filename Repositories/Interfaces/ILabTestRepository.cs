e813d373b3cf3d5cf1a4add35c101b8488c9d42c
Paolagarcia02 <pgpzaragoza@gmail.com>
2026-03-08 10:13:23 +0100

    changes in repositories

--- Repositories/Interfaces/ILabTestRepository.cs
+++ Repositories/Interfaces/ILabTestRepository.cs
@@ -5,6 +5,7 @@ namespace SaviaVetAPI.Repositories
     public interface ILabTestRepository
     {
         public Task<List<GetLabTestDTO>> GetLabTestsAsync();
+        public Task<List<GetLabTestDTO>> GetLabTestsByOwnerIdAsync(int ownerId);
         public Task<GetLabTestDTO> GetOneLabTestAsync(int id);
         public Task<bool> AddLabTestAsync(AddLabTestDTO addLabTestDTO);
         public Task<bool> UpdateLabTestAsync(UpdateLabTestDTO updateLabTestDTO);
